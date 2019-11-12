80fe59403f3b244b133ad414b9f6caf853de4de9
MartinJ95 <46921574+MartinJ95@users.noreply.github.com>
2019-11-12 22:29:35 +0000

    initual setup
    implemented gravity, set up a class for the implementation of a sphere collider

---
+++ Physics_simulation/Assets/RigidBody.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBody : MonoBehaviour
+{
+
+	float gravity = -9.81f;
+	public Vector3 velocity = new Vector3(0, 0, 0);
+	public float mass;
+	public Vector3 force = new Vector3(0, 0, 0);
+	Vector3 calculateForce()
+	{
+		return new Vector3( 0, mass * gravity, 0);
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = calculateForce();
+		Vector3 acceleration = new Vector3(force.x / mass, force.y / mass, force.z / mass);
+		velocity += acceleration * Time.deltaTime;
+		transform.Translate(velocity * Time.deltaTime);
+	}
+}
